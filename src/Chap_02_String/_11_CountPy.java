4bfc28287b5834e2bbe999c771e4bafc04708fd4
yerin03 <kyr2532@naver.com>
2023-07-03 10:43:28 +0900

    #11 solve

--- src/Chap_02_String/_11_CountPy.java
+++ src/Chap_02_String/_11_CountPy.java
@@ -10,17 +10,23 @@ public class _11_CountPy {
        //모든 문자 소문자로 바꾸기
        String lowercase = str.toLowerCase();
        //System.out.println(lowercase);
+       //System.out.println("소문자 변환 : " + lowercase);
 
        //원본길이 저장
        int length = lowercase.length();
+       //System.out.println("원본 길이 : " + length);
 
        //p의 길이 저장
        String changeP = lowercase.replace("p","");
        int P_length = length - changeP.length();
+       //System.out.println("p 바꾼 문자 : " + changeP);
+       //System.out.println("p 갯수 : " + P_length);
 
        //y길이 저장
        String changeY = lowercase.replace("y","");
-       int Y_length = length - changeP.length();
+       int Y_length = length - changeY.length();
+       //System.out.println("y 바꾼 문자 : " + changeY);
+       //System.out.println("y 갯수 : " + Y_length);
 
        //같은지 비교
        boolean equal;
@@ -44,6 +50,61 @@ public class _11_CountPy {
     }
 
 
-
-
 }
+
+
+
+
+/*
+#프로그래머스  코드#
+import java.util.Scanner;
+class Solution {
+    boolean solution(String s) {
+
+        String lowercase = s.toLowerCase();
+        //System.out.println(lowercase);
+        //System.out.println("소문자 변환 : " + lowercase);
+
+        //원본길이 저장
+        int length = lowercase.length();
+        //System.out.println("원본 길이 : " + length);
+
+        //p의 길이 저장
+        String changeP = lowercase.replace("p","");
+        int P_length = length - changeP.length();
+        //System.out.println("p 바꾼 문자 : " + changeP);
+        //System.out.println("p 갯수 : " + P_length);
+
+        //y길이 저장
+        String changeY = lowercase.replace("y","");
+        int Y_length = length - changeY.length();
+        //System.out.println("y 바꾼 문자 : " + changeY);
+        //System.out.println("y 갯수 : " + Y_length);
+
+        //같은지 비교
+        boolean equal;
+
+        if (P_length == Y_length){
+            equal = true;
+        } else {equal = false;}
+
+        return equal;
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        Solution solution = new Solution();
+        boolean TF = solution.solution(str);
+        System.out.println(TF);
+
+    }
+
+
+
+}*/
